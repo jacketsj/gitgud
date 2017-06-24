9e1239566e496171d712519def6192ca3f41c1bc
jacketsj <jacketsj@users.noreply.github.com>
2017-06-24 13:56:15 -0700

    Debugged keywords.py
    Keywords.py is now functional. Added JSON body. Fixed input passing.
    Still needs subscription key.

--- keywords.py
+++ keywords.py
@@ -1,14 +1,25 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 
 def find_keywords(text):
-    headers = { 'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': '{subscription key here}' }
+
+    headers = { 'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': 'subscription key here' }
     
     params = urllib.parse.urlencode({
     })
-
+    
+    body = {
+      "documents": [
+        {
+          "language": "en",
+          "id": "1",
+          "text": text
+          }
+      ]
+    }
+    
     try:
         conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-        conn.request("POST", "/text/analytics/v2.0/keyPhrases?%s" % params, text, headers)
+        conn.request("POST", "/text/analytics/v2.0/keyPhrases?%s" % params, str(body), headers)
         response = conn.getresponse()
         data = response.read()
         conn.close()
